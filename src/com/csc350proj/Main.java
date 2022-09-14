615fd8e819565b2abd3a89ddcf5aff87d49607a9
fherchuk <fherchuk@uri.edu>
2022-09-14 16:42:20 -0400

    Frederick Herchuk added search function to Inventory Class

--- src/com/csc350proj/Main.java
+++ src/com/csc350proj/Main.java
@@ -9,9 +9,21 @@ public class Main {
 
     public static void main(String[] args) {
         //Main Function
-        //Basic Shell for product Class
+        //Testing Filling an inventory object with products
         Inventory inv1 = new Inventory();
         inv1.FillInventory();
+
+
+        //Testing Search Function
+        Integer index = inv1.NameSearch("Coconut");
+
+        //Testing changes to values at the index if it is found by search function based on boolean
+        if (inv1.isFound()) {
+            inv1.getInventoryItems().get(index).setName("Orange");
+            inv1.getInventoryItems().get(index).setPrice(2.10);
+        }
+        //Test print statements for both individual Products and the entire inventory
         inv1.PrintInventory();
+        //inv1.getInventoryItems().get(index).printProduct();
     }
 }
