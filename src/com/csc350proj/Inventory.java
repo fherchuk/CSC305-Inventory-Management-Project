9b7247488e2c6ff582ed2cf84165b892c95b4726
Rick Herchuk <71285964+fherchuk@users.noreply.github.com>
2022-09-26 00:40:13 -0400

    Add files via upload

--- src/com/csc350proj/Inventory.java
+++ src/com/csc350proj/Inventory.java
@@ -1,23 +1,16 @@
 package com.csc350proj;
 
 import java.text.DecimalFormat;
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Inventory {
-    //Inventory Class using Vector.
+    //Inventory Class using HashMap
     // Includes functions for filling and printing...
-    private final Vector<Product> inventoryItems = new Vector<>();
-    private boolean found;
-
-    // Getters Inventory Class
-    public Vector<Product> getInventoryItems() {
-        return inventoryItems;
-    }
-    public boolean isFound(){ return found;    }
+    public final HashMap<String, Product> inventoryItems = new HashMap<>();
 
 
-
-    public void FillInventory(){
+    public void fillInventory() {
         //This is a Function to initially fill the database
         //To Be Referenced later in imports from Excel...
         // TO BE FINISHED
@@ -25,32 +18,45 @@ public class Inventory {
         //Placeholder data
         //Can be changed and altered to test values and new attributes...
         Product temp = new Product("Apple", 0.75, 3);
-        inventoryItems.add(temp);
+        inventoryItems.put(temp.name, temp);
         temp = new Product("Banana", 1.25, 4);
-        inventoryItems.add(temp);
+        inventoryItems.put(temp.name, temp);
         temp = new Product("Coconut", 4.00, 1);
-        inventoryItems.add(temp);
+        inventoryItems.put(temp.name, temp);
     }
 
     // Prints the entirety of the Inventory Vector
-    public void PrintInventory(){
+    public void printInventory() {
         //Test Print Function...
         System.out.println("Here are the following Inventory Items: ");
         DecimalFormat formatPrice = new DecimalFormat("0.00");
-        for (Product product : inventoryItems) {
-            System.out.println(product.getName() + " :: $" + formatPrice.format(product.getPrice()) + " :: " + product.getQuantity());
+        for (Map.Entry<String, Product> index : this.inventoryItems.entrySet()) {
+            System.out.println(index.getKey() + " :: $" + formatPrice.format(index.getValue().price) + " :: " + index.getValue().quantity);
         }
     }
-// Search by name function w/ a found flag boolean in the Inventory Class
-    public Integer NameSearch(String name){
-        for (int i=0; i<inventoryItems.size(); i++) {
-            if(inventoryItems.get(i).getName().equals(name)){
-                found = true;
-                return i;
-            }
+
+    // Creates a new item and adds to the inventory hashmap.
+    public void addItem(String name, Double price, Integer quantity) {
+        Product temp = new Product(name, price, quantity);
+        this.inventoryItems.put(name, temp);
+    }
+
+    public void removeItem(String name) {
+        this.inventoryItems.remove(name);
+    }
+// Replaces attributes of a single item. A new item is created if the key is changed.
+    public void replaceItem(String key, String newName, Double newPrice, Integer newQuantity) {
+        //This statement checks if the key is to be changed and adds a new item with the new key and deletes the old item.
+        if (newName != null) {
+            addItem(newName, this.inventoryItems.get(key).price, this.inventoryItems.get(key).quantity);
+            this.inventoryItems.remove(key);
+            key = newName;
         }
-        System.out.println("Product Not Found");
-        found = false;
-        return 0;
+        if (newPrice != null) { this.inventoryItems.get(key).price = newPrice; }
+        if (newQuantity != null) { this.inventoryItems.get(key).quantity = newQuantity; }
     }
 }
+
+
+
+
